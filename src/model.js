19e4cd8e74f64ef77be8679f7469f6ac976ceda2
Asmaa-Edris <asmaaedris928@gmail.com>
2025-10-31 23:34:47 +0200

    model.js

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default function Model({isVisable,errorMassage="null"}) {
 
   if(isVisable){
@@ -17,23 +17,4 @@ export default function Model({isVisable,errorMassage="null"}) {
     return(<></>)
   }
 }
-=======
-export default function Model({isVisable,errorMassage="null"}) {
 
-  if(isVisable){
-
-    return (
-<div id="model">
- <div className="model-content">
- <h2 style={{color :errorMassage ?"red":"green"} }>
-{errorMassage !=null?errorMassage:"The Form Has Been Submitted Successfully"}
-</h2>
-</div>
- </div>
-    );
-  }
-  else{
-    return(<></>)
-  }
-}
->>>>>>> 4651cceef8a6619d87d39d2e34cd4e8835cae5d6
